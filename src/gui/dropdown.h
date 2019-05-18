d6454a32a28968ea2cea1afff2cd5381ec8e1641
Michael Hept <michael.hept@gmail.com>
2019-05-18 17:44:03 +0200

    Disable scissoring on dropdowns

--- src/gui/dropdown.h
+++ src/gui/dropdown.h
@@ -15,8 +15,10 @@ public:
 	guidropdownbase() : guibuttonbase() {
 	}
 	void render(NVGcontext* vg) {
+//		nvgIntersectScissor(vg, pos.x, pos.y, size.x, size.y);
 		renderWidgetBorder(vg, getStateFlags());
-		setFont(vg, G_FONT_SCALE(size.y), G_WHITE, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
+		int fontScale = math::round((this->fontSize > 0 ? this->fontSize : size.y) * fFontScale);
+		setFont(vg, fontScale, G_WHITE, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
 		nvgText(vg, pos.x + size.x / 2.0f, pos.y + G_FONT_MIDDLE_OFFSET(size.y), StringAsCStr(getString()), NULL);
 	}
 	virtual void handleDraggedRelease(MouseEvent& evt) {
