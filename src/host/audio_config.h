c4952258c7257136769c9a1505e40ceaac1b27b6
Michael Hept <michael.hept@gmail.com>
2019-07-08 15:23:20 +0200

    audio_config.h header

---
+++ src/host/audio_config.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "str_util.h"
+#include <stdint.h>
+#include <vector>
+namespace AudioIO {
+	enum tracktype {
+		MONO, STEREO, MULTI_CHANNEL_4, MULTI_CHANNEL_6
+	};
+	struct io_cfg_channel {
+		String name;
+		int32_t idx = -1;
+		int32_t channelOffset = -1;
+		tracktype type = MONO;
+	};
+	struct io_cfg_tracks {
+		bool isInit = false;
+		std::vector<io_cfg_channel> input;
+		std::vector<io_cfg_channel> output;
+	};
+	int32_t getNumChannelsTrackType(tracktype t);
+	tracktype getTrackTypeNumChannels(int32_t t);
+
+	String getTrackNameShort(AudioIO::tracktype type, int32_t index, bool isInput);
+	String getTrackName(AudioIO::tracktype type, int32_t index, bool isInput);
+}
