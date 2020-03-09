06ed50758848cbd1c0ffab75630de959d0e0644f
Michael Hept <michael.hept@gmail.com>
2020-03-09 17:55:58 +0100

    Adjust meter width

--- src/gui/trackcontrols.cpp
+++ src/gui/trackcontrols.cpp
@@ -829,7 +829,7 @@ public:
 		int32_t i2 = inset * 2;
 		int32_t h = TRACK_HEIGHT_STEP-i2;
 
-		int32_t mW = TRACK_HEIGHT_STEP;
+		int32_t mW = TRACK_HEIGHT_STEP*3;
 		int32_t bW = size.x-mW;
 		int32_t gW = size.x-mW;
 		btnBypass.size = ivec2(bW - inset * 3 - h, h);
