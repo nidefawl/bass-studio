047e396104cd42eceb9934e89feb744657e22635
Michael Hept <michael.hept@gmail.com>
2020-03-19 22:40:42 +0100

    Update RGB LED UPD protocol

--- src/host/main_rgb_master.cpp
+++ src/host/main_rgb_master.cpp
@@ -96,7 +96,7 @@ void network_cleanup(void);
 // */
 #include <stdio.h>
 
-#define MAXBUF	10 * 1024
+#define MAXBUF	32 * 1024
 
 
 int mainApp(int argc, char **argv);
@@ -107,25 +107,80 @@ int main(int argc, char **argv)
 	network_cleanup();
 }
 
-#define OPMODE_SLAVE_LISTEN 0
+
+#define LED_PIN 13
+#define NUM_LEDS 30
+#define BRIGHTNESS          35 //255 is max, MAKE SURE YOUR PSU CAN PROVIDE THE AMPS!
+#define LAMP_ID 2
+
+
+#define OPMODE_DISPLAY 0
 #define OPMODE_FADE 1
 #define OPMODE_ON 2
 #define OPMODE_OFF 3
-#define PKT_TYPE_SET_RGB 0
-#define PKT_TYPE_SET_OPMODE 1
-#define PKT_TYPE_REFRESH 2
-#define MAX_LEDS 256
+#define PKT_TYPE_SET_CFG 1
+#define PKT_TYPE_SET_RGB 2
+#define PKT_TYPE_REFRESH 3
+
+#define CFG_ID_COUNT 7
+
+#define NUM_STRIPES 5U
+#define FRAME_LEN NUM_LEDS
+#define FRAME_SIZE_BYTES (FRAME_LEN*sizeof(int32_t))
+#define FRAME_ARRAY_LEN 16U
+
+const char *ssid = "ddwrt"; // The SSID (name) of the Wi-Fi network you want to connect to
+const char *password = "wifipassword";  // The password of the Wi-Fi network
+;
 
 struct heartbeat_message {
   uint32_t client_id;
   uint32_t chk;
 };
-struct led_command {
-  uint8_t type;
-  uint8_t opmode;
-  uint8_t pos;
-  uint8_t len;
+
+struct packet_hdr_t {
+  uint16_t packetType;
+  uint16_t len;
 };
+
+struct frame_hdr_t {
+  uint16_t numFrames;
+  uint16_t frameSizeBytes;
+};
+
+enum cfg_type {
+  CFG_ID_LEDPIN = 0,
+  CFG_ID_BRIGHTNESS,
+  CFG_ID_LAMPID,
+  CFG_ID_NUMLEDCONNECTED,
+  CFG_ID_HEARTBEATINTERVAL,
+  CFG_ID_FRAME_DURATION,
+  CFG_ID_OPMODE
+};
+#define CFG_ID_COUNT (((int)CFG_ID_OPMODE)+1)
+
+struct lamp_config_t {
+  uint32_t frameDuration_millis;
+  uint32_t heartBeatInterval_millis;
+  uint32_t lampId;
+  uint32_t numLEDConnected;
+  uint8_t ledPin;
+  uint8_t brightness;
+  uint8_t opMode;
+};
+
+struct lamp_state_t {
+  bool opModeChanged;
+  uint16_t writePosArrayIdx;
+  uint16_t readPosArrayIdx;
+  unsigned long lastFrameTime;
+  unsigned long lastReceived;
+  unsigned long lastHeartBeatSent;
+};
+
+uint32_t rgb_tempbuffer[FRAME_LEN];
+uint32_t rgb_buffer_array[FRAME_ARRAY_LEN][FRAME_LEN];
+static char buf[MAXBUF];
 int mainApp(int argc, char **argv)
 {
 
@@ -175,8 +230,9 @@ int mainApp(int argc, char **argv)
 
 
 	struct sockaddr *cliaddr = (struct sockaddr*) malloc(addrLen);
-	char buf[MAXBUF];
-	int ledRGBArray[MAX_LEDS];
+	int ledRGBArray[NUM_LEDS];
+	std::map<int32_t, lamp_config_t> configs;
+
 	n = 0;
 	for (;;) { /* do forever */
 		int rc;
@@ -194,45 +250,106 @@ int mainApp(int argc, char **argv)
 
 		heartbeat_message pktHeartbeat;
 		memcpy(&pktHeartbeat, buf, sizeof(heartbeat_message));
-		const uint8_t lampId = pktHeartbeat.client_id;
-		const uint8_t numLeds = pktHeartbeat.chk > MAX_LEDS ? MAX_LEDS: pktHeartbeat.chk;
-//		printf("sendto %d\n", (int64_t)sockfd);
-		led_command ledCommand = { PKT_TYPE_SET_OPMODE, OPMODE_SLAVE_LISTEN, 0, 0 };
-		memcpy(buf, (void*) &ledCommand, sizeof(led_command));
-		if (sendto(sockfd, buf, sizeof(led_command), 0, cliaddr, addrLen) < 0) {
-			if (errno == ENOBUFS)
-				continue;
-			printf("server error: errno %d\n", errno);
-			break;
-		}
-		ledCommand = { PKT_TYPE_SET_RGB, OPMODE_SLAVE_LISTEN, 0, numLeds };
-		memcpy(buf, (void*) &ledCommand, sizeof(led_command));
-		for (int i = 0; i < numLeds; i++) {
-			uint32_t rgbVal = 0x996633;
-			rgbVal &= 0x0;
-			rgbVal |= rand()&0xFF;
-			rgbVal <<= 8;
-//			rgbVal |= rand()&0xFF;
-			rgbVal <<= 8;
-//			rgbVal |= rand()&0xFF;
-			ledRGBArray[i] = rgbVal;
-		}
-		memcpy(buf+sizeof(led_command), (void*) ledRGBArray, sizeof(uint32_t)*numLeds);
-		if (sendto(sockfd, buf, sizeof(led_command)+sizeof(uint32_t)*numLeds, 0, cliaddr, addrLen) < 0) {
-			if (errno == ENOBUFS)
-				continue;
-			printf("server error: errno %d\n", errno);
-			break;
-		}
-		ledCommand = { PKT_TYPE_REFRESH, brightness, 0, 255 };
-		memcpy(buf, (void*) &ledCommand, sizeof(led_command));
-		if (sendto(sockfd, buf, sizeof(led_command), 0, cliaddr, addrLen) < 0) {
-			if (errno == ENOBUFS)
-				continue;
-			printf("server error: errno %d\n", errno);
-			break;
-		}
+		const int16_t lampId = pktHeartbeat.client_id;
+		if (0 == configs.count(lampId)) {
+			{
+				packet_hdr_t hdr;
+				hdr.len = sizeof(lamp_config_t);
+				hdr.packetType = PKT_TYPE_SET_CFG;
 
+				lamp_config_t cfg;
+				cfg.opMode = OPMODE_DISPLAY;
+				cfg.brightness = 100;
+				cfg.heartBeatInterval_millis = 200;
+				cfg.frameDuration_millis = 50;
+				uint32_t mask = 1<<(int)CFG_ID_BRIGHTNESS|1<<(int)CFG_ID_HEARTBEATINTERVAL|1<<(int)CFG_ID_FRAME_DURATION;
+				configs[lampId] = cfg;
+				char* bufPos = buf;
+				memcpy(bufPos, (void*) &hdr, sizeof(packet_hdr_t));
+				bufPos+=sizeof(packet_hdr_t);
+				memcpy(bufPos, (void*) &mask, sizeof(uint32_t));
+				bufPos+=sizeof(uint32_t);
+				memcpy(bufPos, (void*) &cfg, sizeof(lamp_config_t));
+				bufPos+=sizeof(lamp_config_t);
+				printf("sendto %d\n", bufPos-buf);
+				if (sendto(sockfd, buf, bufPos-buf, 0, cliaddr, addrLen) < 0) {
+					int curErrNo = errno;
+
+					printf("server error: errno %d\n", curErrNo);
+					if (curErrNo == ENOBUFS)
+						continue;
+					break;
+				}
+			}
+			{
+				packet_hdr_t hdr;
+				hdr.len = sizeof(lamp_config_t);
+				hdr.packetType = PKT_TYPE_SET_RGB;
+				frame_hdr_t frameHdr;
+				frameHdr.numFrames = 4;
+				frameHdr.frameSizeBytes = FRAME_SIZE_BYTES;
+				auto arr = new int[NUM_LEDS];
+				for (int i = 0; i < NUM_LEDS; i++) {
+					arr[i] = 0xFF996633;
+				}
+				char* bufPos = buf;
+				memcpy(bufPos, (void*) &hdr, sizeof(packet_hdr_t));
+				bufPos+=sizeof(packet_hdr_t);
+				memcpy(bufPos, (void*) &frameHdr, sizeof(frame_hdr_t));
+				bufPos+=sizeof(frame_hdr_t);
+				memcpy(bufPos, (void*) &arr[0], sizeof(int32_t)*NUM_LEDS);
+				bufPos+=sizeof(int32_t)*NUM_LEDS;
+				delete[] arr;
+				printf("sendto2 %d\n", bufPos-buf);
+				if (sendto(sockfd, buf, bufPos-buf, 0, cliaddr, addrLen) < 0) {
+					int curErrNo = errno;
+
+					printf("server error: errno %d\n", curErrNo);
+					if (curErrNo == ENOBUFS)
+						continue;
+					break;
+				}
+			}
+		}
+//		const int16_t numLeds = pktHeartbeat.chk > NUM_LEDS ? NUM_LEDS: pktHeartbeat.chk;
+////		printf("sendto %d\n", (int64_t)sockfd);
+//		led_command ledCommand = { PKT_TYPE_SET_OPMODE, OPMODE_SLAVE_LISTEN, 0, 0 };
+//		memcpy(buf, (void*) &ledCommand, sizeof(led_command));
+//		if (sendto(sockfd, buf, sizeof(led_command), 0, cliaddr, addrLen) < 0) {
+//			if (errno == ENOBUFS)
+//				continue;
+//			printf("server error: errno %d\n", errno);
+//			break;
+//		}
+//		ledCommand = { PKT_TYPE_SET_RGB, OPMODE_SLAVE_LISTEN, 0, (uint8_t)numLeds };
+//		memcpy(buf, (void*) &ledCommand, sizeof(led_command));
+//		int n = rand()%(numLeds/2);
+//		for (int i = 0; i < numLeds; i++) {
+//			uint32_t rgbVal = 0x996633;
+//			rgbVal &= 0x0;
+//			rgbVal |= math::min(255, math::max(0, (math::max(0, i-n)*455/(numLeds-1))))&0xFF;
+//			rgbVal <<= 8;
+//			rgbVal |= math::min(255, math::max(0, (math::max(0, i-n)*355/(numLeds-1))))&0xFF;
+//			rgbVal <<= 8;
+//			rgbVal |= math::min(255, math::max(0, (math::max(0, i-n)*255/(numLeds-1))))&0xFF;
+//			ledRGBArray[i] = rgbVal;
+//		}
+//		memcpy(buf+sizeof(led_command), (void*) ledRGBArray, sizeof(uint32_t)*numLeds);
+//		if (sendto(sockfd, buf, sizeof(led_command)+sizeof(uint32_t)*numLeds, 0, cliaddr, addrLen) < 0) {
+//			if (errno == ENOBUFS)
+//				continue;
+//			printf("server error: errno %d\n", errno);
+//			break;
+//		}
+//		ledCommand = { PKT_TYPE_REFRESH, brightness, 0, 255 };
+//		memcpy(buf, (void*) &ledCommand, sizeof(led_command));
+//		if (sendto(sockfd, buf, sizeof(led_command), 0, cliaddr, addrLen) < 0) {
+//			if (errno == ENOBUFS)
+//				continue;
+//			printf("server error: errno %d\n", errno);
+//			break;
+//		}
+//
 
 	}
 	/* can't get here, but just in case: close sockets
