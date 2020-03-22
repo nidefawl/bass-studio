81340bcc5f6eb47ce6be4cb865d24e5d3635ced3
Michael Hept <michael.hept@gmail.com>
2020-03-22 11:54:26 +0100

    Trying different protocol to overcome very unreliable UDP over WIFI transmission

--- src/host/main_rgb_master.cpp
+++ src/host/main_rgb_master.cpp
@@ -9,6 +9,9 @@
 #include <algorithm>
 #include <atomic>
 #include "basectrl.h"
+#include "color_util.h"
+#include "platform.h"
+#include "threads.h"
 #include "assert_dbg.h"
 
 #ifdef _WIN32
@@ -108,12 +111,6 @@ int main(int argc, char **argv)
 }
 
 
-#define LED_PIN 13
-#define NUM_LEDS 30
-#define BRIGHTNESS          35 //255 is max, MAKE SURE YOUR PSU CAN PROVIDE THE AMPS!
-#define LAMP_ID 2
-
-
 #define OPMODE_DISPLAY 0
 #define OPMODE_FADE 1
 #define OPMODE_ON 2
@@ -125,17 +122,28 @@ int main(int argc, char **argv)
 #define CFG_ID_COUNT 7
 
 #define NUM_STRIPES 5U
-#define FRAME_LEN NUM_LEDS
-#define FRAME_SIZE_BYTES (FRAME_LEN*sizeof(int32_t))
-#define FRAME_ARRAY_LEN 16U
+#define NUM_LEDS 30U
+#define NUM_TOTAL_LEDS (NUM_STRIPES*NUM_LEDS)
+#define BRIGHTNESS          35 //255 is max, MAKE SURE YOUR PSU CAN PROVIDE THE AMPS!
+#define LAMP_ID 2
+
+
+#define FRAME_SIZE_BYTES (NUM_TOTAL_LEDS*sizeof(int32_t))
+#define FRAME_ARRAY_LEN 64U
+
+
+// MASTER SIDE PARAMETERS
+#define FRAME_DURATION 24
+#define FRAME_BUF_LEN 2
 
 const char *ssid = "ddwrt"; // The SSID (name) of the Wi-Fi network you want to connect to
 const char *password = "wifipassword";  // The password of the Wi-Fi network
 ;
 
 struct heartbeat_message {
-  uint32_t client_id;
-  uint32_t chk;
+  uint32_t packetsReceived;
+  uint8_t lampId;
+  uint8_t opMode;
 };
 
 struct packet_hdr_t {
@@ -144,6 +152,7 @@ struct packet_hdr_t {
 };
 
 struct frame_hdr_t {
+  uint16_t frameId;
   uint16_t numFrames;
   uint16_t frameSizeBytes;
 };
@@ -168,6 +177,11 @@ struct lamp_config_t {
   uint8_t brightness;
   uint8_t opMode;
 };
+struct lamp_context_t {
+	uint64_t frameId = 0;
+	uint64_t frameStep = 0;
+	uint64_t packetId = 0;
+};
 
 struct lamp_state_t {
   bool opModeChanged;
@@ -178,12 +192,15 @@ struct lamp_state_t {
   unsigned long lastHeartBeatSent;
 };
 
-uint32_t rgb_tempbuffer[FRAME_LEN];
-uint32_t rgb_buffer_array[FRAME_ARRAY_LEN][FRAME_LEN];
 static char buf[MAXBUF];
 int mainApp(int argc, char **argv)
 {
 
+	printf("sizeof(struct packet_hdr_t) %d\n", sizeof(struct packet_hdr_t));
+	printf("sizeof(struct heartbeat_message) %d\n", sizeof(struct heartbeat_message));
+	  printf("sizeof(struct frame_hdr_t) %d\n", sizeof(struct frame_hdr_t));
+	  printf("sizeof(struct lamp_config_t) %d\n", sizeof(struct lamp_config_t));
+	  printf("sizeof(struct uint32_t) %d\n", sizeof(uint32_t));
 	/* Check for arguments - hostname and portnumber required */
 	if (argc<3) {
 		printf("Too few arguments\n");
@@ -230,10 +247,11 @@ int mainApp(int argc, char **argv)
 
 
 	struct sockaddr *cliaddr = (struct sockaddr*) malloc(addrLen);
-	int ledRGBArray[NUM_LEDS];
 	std::map<int32_t, lamp_config_t> configs;
+	std::map<int32_t, lamp_context_t> ctxts;
 
 	n = 0;
+	int masterFrameStep = 0;
 	for (;;) { /* do forever */
 		int rc;
 
@@ -250,19 +268,24 @@ int mainApp(int argc, char **argv)
 
 		heartbeat_message pktHeartbeat;
 		memcpy(&pktHeartbeat, buf, sizeof(heartbeat_message));
-		const int16_t lampId = pktHeartbeat.client_id;
-		if (0 == configs.count(lampId)) {
+		const int16_t lampId = pktHeartbeat.lampId;
+		printf("LAMP %d: Packets received %d\n", lampId, pktHeartbeat.packetsReceived);
+		if (0==ctxts.count(lampId)) {
+			ctxts[lampId] = lamp_context_t{};
+
+		}
+		if (0==configs.count(lampId) || pktHeartbeat.opMode != OPMODE_DISPLAY) {
 			{
 				packet_hdr_t hdr;
-				hdr.len = sizeof(lamp_config_t);
+				hdr.len = sizeof(uint32_t)+sizeof(lamp_config_t);
 				hdr.packetType = PKT_TYPE_SET_CFG;
 
 				lamp_config_t cfg;
 				cfg.opMode = OPMODE_DISPLAY;
-				cfg.brightness = 100;
-				cfg.heartBeatInterval_millis = 200;
-				cfg.frameDuration_millis = 50;
-				uint32_t mask = 1<<(int)CFG_ID_BRIGHTNESS|1<<(int)CFG_ID_HEARTBEATINTERVAL|1<<(int)CFG_ID_FRAME_DURATION;
+				cfg.brightness = 70;
+				cfg.heartBeatInterval_millis = 5000;
+				cfg.frameDuration_millis = FRAME_DURATION;
+				uint32_t mask = (1<<(int)CFG_ID_OPMODE)|(1<<(int)CFG_ID_BRIGHTNESS)|(1<<(int)CFG_ID_HEARTBEATINTERVAL)|(1<<(int)CFG_ID_FRAME_DURATION);
 				configs[lampId] = cfg;
 				char* bufPos = buf;
 				memcpy(bufPos, (void*) &hdr, sizeof(packet_hdr_t));
@@ -281,33 +304,132 @@ int mainApp(int argc, char **argv)
 					break;
 				}
 			}
+			printf("send frames to lamp %d\n", lampId);
+			uint64_t tmStart = getTimeMillis();
+			uint64_t tmStartHb = getTimeMillis();
+			for (;;)
 			{
-				packet_hdr_t hdr;
-				hdr.len = sizeof(lamp_config_t);
-				hdr.packetType = PKT_TYPE_SET_RGB;
-				frame_hdr_t frameHdr;
-				frameHdr.numFrames = 4;
-				frameHdr.frameSizeBytes = FRAME_SIZE_BYTES;
-				auto arr = new int[NUM_LEDS];
-				for (int i = 0; i < NUM_LEDS; i++) {
-					arr[i] = 0xFF996633;
-				}
-				char* bufPos = buf;
-				memcpy(bufPos, (void*) &hdr, sizeof(packet_hdr_t));
-				bufPos+=sizeof(packet_hdr_t);
-				memcpy(bufPos, (void*) &frameHdr, sizeof(frame_hdr_t));
-				bufPos+=sizeof(frame_hdr_t);
-				memcpy(bufPos, (void*) &arr[0], sizeof(int32_t)*NUM_LEDS);
-				bufPos+=sizeof(int32_t)*NUM_LEDS;
-				delete[] arr;
-				printf("sendto2 %d\n", bufPos-buf);
-				if (sendto(sockfd, buf, bufPos-buf, 0, cliaddr, addrLen) < 0) {
-					int curErrNo = errno;
 
-					printf("server error: errno %d\n", curErrNo);
-					if (curErrNo == ENOBUFS)
-						continue;
-					break;
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+				uint64_t tmNow = getTimeMillis();
+				uint64_t millisd = tmNow-tmStart;
+				uint64_t millisHb = tmNow-tmStartHb;
+
+				if (millisHb >= configs[lampId].heartBeatInterval_millis) {
+					tmStartHb = getTimeMillis();
+
+					if ((rc = recvfrom(sockfd, buf, MAXBUF, 0, cliaddr, &addrLen)) < 0) {
+						printf("server error: errno %d\n", errno);
+						perror("reading datagram");
+						break;
+					}
+					if (n < 10) {
+						printf("recvfrom %d len %d\n", (int64_t)rc, addrLen);
+					}
+					n++;
+
+
+					heartbeat_message pktHeartbeat;
+					memcpy(&pktHeartbeat, buf, sizeof(heartbeat_message));
+					const int16_t lampId = pktHeartbeat.lampId;
+					printf("LAMP %d: Packets received %d\n", lampId, pktHeartbeat.packetsReceived);
+					if (0==ctxts.count(lampId)) {
+						ctxts[lampId] = lamp_context_t{};
+
+					}
+				}
+				if (millisd >= FRAME_DURATION*FRAME_BUF_LEN) {
+					printf("send frames...\n");
+					tmStart = getTimeMillis();
+
+
+					lamp_context_t& ctxt = ctxts[lampId];
+					ctxt.frameStep = masterFrameStep;
+					packet_hdr_t hdr;
+					hdr.len = sizeof(lamp_config_t);
+					hdr.packetType = PKT_TYPE_SET_RGB;
+					frame_hdr_t frameHdr;
+					frameHdr.frameId = ctxt.frameId;
+					frameHdr.numFrames = FRAME_BUF_LEN;
+					frameHdr.frameSizeBytes = FRAME_SIZE_BYTES;
+					dbgassert (sizeof(int32_t)*NUM_TOTAL_LEDS == FRAME_SIZE_BYTES);
+					char* bufPos = buf;
+					memcpy(bufPos, (void*) &hdr, sizeof(packet_hdr_t));
+					bufPos+=sizeof(packet_hdr_t);
+					memcpy(bufPos, (void*) &frameHdr, sizeof(frame_hdr_t));
+					bufPos+=sizeof(frame_hdr_t);
+					auto arr = new int[NUM_TOTAL_LEDS];
+					for (int frameNr = 0; frameNr < FRAME_BUF_LEN; frameNr++) {
+						ctxt.frameId++;
+						ctxt.frameStep++;
+						float ff = fmodf(ctxt.frameStep/20.0f, 1.0f);
+						float ff2 = fmodf(ctxt.frameStep/7.0f, 1.0f);
+						ff2 = 1.0f-abs(ff2-0.5f);
+						ff = 1.0f-abs(ff-0.5f);
+						ff *= ff;
+						for (int i = 0; i < NUM_TOTAL_LEDS; i++) {
+							int x = i%NUM_LEDS;
+							int y = i/NUM_LEDS;
+							if (lampId < 2 && ctxts.size() > 1) {
+								y += NUM_STRIPES;
+							}
+							float fx = x/(float)(NUM_LEDS-1);
+
+							float fy = y/(float)((NUM_STRIPES*2)-1);
+		//					if (ctxts.size() > 1) {
+		//						fy = fy*0.5f;
+		//						if (lampId < 2) {
+		//							fy += 0.5f;
+		//						}
+		//					}
+							float cntx = (1.0f-abs(fx-0.5f));
+							float cnty = (1.0f-abs(fy-0.5f));
+							float b = pow(cntx, 1.0f+7.0f*ff)*pow(cnty, 1.0f+7.0f*ff);
+							float g = pow(cntx, 1.8f+9.0f*ff)*pow(cnty, 1.6f+3.0f*ff2);
+							float r = pow(cntx, 1.2f+2.0f*ff2)*pow(cnty, 1.0f+7.0f*ff);
+							g = 0;
+							r = 0;
+							float sc = 0.3f;
+							r*=sc;
+							g*=sc;
+							b*=sc;
+							float fColHue = (cnty*cntx) + ctxt.frameStep*0.01f;
+							NVGcolor col = HSLtoRGB(fmodf(fColHue, 1.0f), 1.0f, 1.0f);
+							r = col.r;
+							g = col.g;
+							b = col.b;
+							int intRGBA = math::max<int32_t>(0, math::min<int32_t>(255, math::round(b*255.0f)));
+							intRGBA |= (math::max<int32_t>(0, math::min<int32_t>(255, math::round(g*255.0f)))&0xFF)<<8;
+							intRGBA |= (math::max<int32_t>(0, math::min<int32_t>(255, math::round(r*255.0f)))&0xFF)<<16;
+
+
+							arr[i] = intRGBA;
+//							if ((ctxt.frameStep)%NUM_TOTAL_LEDS == i+(lampId-1)*NUM_LEDS) {
+//								arr[i] = 0x2288dd;
+//							}
+//							if (i < lampId && ((ctxt.frameStep/6)%6) < 6/3) {
+//								arr[i] = 0x770077;
+//							}
+						}
+						memcpy(bufPos, (void*) &arr[0], sizeof(int32_t)*NUM_TOTAL_LEDS);
+						bufPos+=sizeof(int32_t)*NUM_TOTAL_LEDS;
+						if (lampId <= 1) {
+							masterFrameStep = ctxt.frameStep;
+						}
+					}
+					delete[] arr;
+					ctxt.packetId++;
+					printf("send lampId %d, packetId %d, frameId %d size %d\n", lampId, ctxt.packetId, frameHdr.frameId, ((size_t)bufPos-(size_t)buf));
+					for (int i = 0; i < 1; i++) {
+						if (sendto(sockfd, buf, bufPos-buf, 0, cliaddr, addrLen) < 0) {
+							int curErrNo = errno;
+
+							printf("server error: errno %d\n", curErrNo);
+							if (curErrNo == ENOBUFS)
+								continue;
+							break;
+						}
+					}
 				}
 			}
 		}
