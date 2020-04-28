2270ffd0ec1056ba22dacf7f15b987b269ccc75f
Michael Hept <michael.hept@gmail.com>
2020-04-28 19:57:04 +0200

    MsgCounter is _WIN32 exclusive

--- src/gui/debugctr.cpp
+++ src/gui/debugctr.cpp
@@ -71,8 +71,9 @@ gui_ctr_debug::gui_ctr_debug(gui_ctr_debug_type_i32 debugCtrType) :
 		guictr_base(),
 		impl(new gui_ctr_debug::ctr_debug_impl_t{}),
 		dgbCtrType(debugCtrType) {
-
+#ifdef _WIN32
 	msgCounterEnabled=true;
+#endif
 	setBackgroundRendered(true);
 	std::vector<guibase*>& debugGuis = impl->debugGuis;
 	if (dgbCtrType != gui_ctr_debug_type_i32::TYPE_2) {
