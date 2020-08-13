289af51f06380090b995fe87d2044ba11e390ef9
Michael Hept <michael.hept@gmail.com>
2020-08-13 18:24:53 +0200

    Set non zero default value for folded vertical note view scale

--- src/include/layout.h
+++ src/include/layout.h
@@ -5,7 +5,7 @@ struct layout_pianoroll_t {
 	float yscale = 10.0f;
 	float yoffset = 0.0f;
 	bool fold = false;
-	float yscalefold = 0.0f;
+	float yscalefold = 10.0f;
 	float yoffsetfold = 0.0f;
 	float& offset() {
 		return fold ? this->yoffsetfold : this->yoffset;
