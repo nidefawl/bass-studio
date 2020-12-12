46bde892fffe16a926117be5b1d97c465439d8d4
Michael Hept <michael.hept@gmail.com>
2020-12-12 11:48:51 +0100

    Missing change for workaround for std::this_thread::get_id() not providing arithmetic identifier

--- src/cli/console/console_thread.cpp
+++ src/cli/console/console_thread.cpp
@@ -39,7 +39,7 @@ public:
 			this->run();
 		});
 	#ifdef _WIN32
-			this->threadid = static_cast<int32_t>(t.get_id().get());
+			this->threadid = static_cast<int32_t>(get_thread_id());
 //			HANDLE h = reinterpret_cast<HANDLE*>(t.native_handle());
 	#endif
 	}
