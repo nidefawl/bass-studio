3232770e7ac8567ee9224de1a075f895bfabc33a
Michael Hept <michael.hept@gmail.com>
2021-07-28 03:09:59 +0200

    Refactor cmake scripts

--- setup.cmake
+++ setup.cmake
@@ -6,11 +6,11 @@ set(LINUX TRUE)
 endif()
 
 FUNCTION(PREPEND var prefix)
-   SET(listVar "")
-   FOREACH(f ${ARGN})
-      LIST(APPEND listVar "${prefix}/${f}")
-   ENDFOREACH(f)
-   SET(${var} "${listVar}" PARENT_SCOPE)
+  SET(listVar "")
+  FOREACH(f ${ARGN})
+    LIST(APPEND listVar "${prefix}${f}")
+  ENDFOREACH(f)
+  SET(${var} "${listVar}" PARENT_SCOPE)
 ENDFUNCTION(PREPEND)
 
 FUNCTION(ADD_POST_BUILD_COMMANDS targetBuildName)
@@ -29,6 +29,32 @@ FUNCTION(ADD_POST_BUILD_COMMANDS targetBuildName)
       )
     endif()
   endif()
+ENDFUNCTION()
+# macro to add define a executable binary to be built
+FUNCTION(SET_APP_BUILD appname)
+  set(tmp1 "${appname}-${BUILD_BINARY_SUFFIX}")
+  set_target_properties(${appname} PROPERTIES OUTPUT_NAME ${tmp1})
+  set_target_properties(${appname} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${APP_WORKING_DIR}$<$<CONFIG:Debug>:>)
+  set_target_properties(${appname} PROPERTIES VS_DEBUGGER_WORKING_DIRECTORY ${APP_WORKING_DIR})
+  set_target_properties(${appname} PROPERTIES OUTPUT_NAME_DEBUG "${appname}-${CMAKE_CXX_COMPILER_ID}-debug${OUTPUT_BINARY_SUFFIX}" )
+  set_target_properties(${appname} PROPERTIES OUTPUT_NAME_RELEASE "${appname}-${CMAKE_CXX_COMPILER_ID}-release${OUTPUT_BINARY_SUFFIX}" )
+  set_target_properties(${appname} PROPERTIES OUTPUT_NAME_MINSIZEREL "${appname}-${CMAKE_CXX_COMPILER_ID}-minsizerel${OUTPUT_BINARY_SUFFIX}" )
+  set_target_properties(${appname} PROPERTIES OUTPUT_NAME_RELWITHDEBINFO "${appname}-${CMAKE_CXX_COMPILER_ID}-relwithdebinfo${OUTPUT_BINARY_SUFFIX}" )
+ENDFUNCTION(SET_APP_BUILD)
+
+FUNCTION(GENERATE_BUILDINFO_CPP)
+  # process variables from CMAKE build system towards a buildinfo.cpp file
+  # this only happens at configuration and requires manual deletion of buildinfo.cpp to reflect the latest options
+  get_property(BUILD_COMPILE_OPTIONS        DIRECTORY  PROPERTY COMPILE_OPTIONS)
+  get_property(BUILD_COMPILE_DEFS           DIRECTORY  PROPERTY COMPILE_DEFINITIONS)
+  get_property(BUILD_CXX_STANDARD           TARGET ${BUILD_NAME}  PROPERTY CXX_STANDARD)
+  string(TOUPPER ${CMAKE_BUILD_TYPE} BUILD_TYPE_SUFFIX)
+  separate_arguments(GLOBAL_FLAGS UNIX_COMMAND "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${BUILD_TYPE_SUFFIX}}")
+  list(APPEND BUILD_COMPILE_OPTIONS ${GLOBAL_FLAGS})
+  list(APPEND BUILD_COMPILE_OPTIONS -std=c++${BUILD_CXX_STANDARD})
+  string(REPLACE ";" " " BUILD_COMPILE_OPTIONS "${BUILD_COMPILE_OPTIONS}")
+  string(REPLACE ";" " -D" BUILD_COMPILE_DEFS "-D${BUILD_COMPILE_DEFS}")
+  CONFIGURE_FILE( ${DAW_SRC_PATH}/app/buildinfo.cpp.in ${CMAKE_BINARY_DIR}/buildinfo.cpp ESCAPE_QUOTES)
 ENDFUNCTION()
 
 if (NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
@@ -56,19 +82,7 @@ endif()
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
-
-#SET(DAW_COMPILE_FLAGS VST_FORCE_DEPRECATED=0 USE_GLAD_GL_HEADERS GLM_FORCE_CXX14=1 PA_ENABLE_DEBUG_OUTPUT=1)
-SET(DAW_COMPILE_FLAGS VST_FORCE_DEPRECATED=0 USE_GLAD_GL_HEADERS GLM_FORCE_CXX14=1)
-if (WIN32)
-  SET(DAW_COMPILE_FLAGS ${DAW_COMPILE_FLAGS} PA_USE_ASIO=1 PA_USE_DS=1 PA_USE_WASAPI=1 PA_USE_WDMKS=1)
-endif(WIN32)
-if (UNIX)
-  SET(DAW_COMPILE_FLAGS ${DAW_COMPILE_FLAGS} PA_USE_ALSA=1)
-	set(DAW_COMPILE_FLAGS ${DAW_COMPILE_FLAGS} __cdecl="")
-endif(UNIX)
-
 OPTION(DEBUG_STD_LIB "Enable standard library assertions" OFF) # Enabled by default
-
 
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
   add_compile_options(-Wall -Wno-inconsistent-missing-override -Wno-unused-parameter) # -Wshadow sadly no working option for warning on shadow local only
@@ -96,33 +110,35 @@ elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 endif()
 set(IS_MINGW_BUILD OFF)
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-  if (NOT WIN32)  #cant use stack-protector with clang/gnu toolchain on windows
+  # can't use stack-protector with clang/gnu toolchain on windows
+  if (NOT WIN32)  
+    # stack protector improves debugging corrupted stacks on linux
     add_compile_options(-fstack-protector)
   else() 
     set(IS_MINGW_BUILD ON)
     add_compile_options(-gdwarf-3) #cv2pdb does not produce correct filenames/lines with dwarf > 3
   #  add_compile_definitions(_MSVCRT_VERSION__=0x1200) #link against msvcr120 runtime
   endif()
+  # improve debugging
   add_compile_options(-fno-omit-frame-pointer)
 endif()
-
-string(REPLACE " " ";" DAW_FLAG_LIST ${DAW_COMPILE_FLAGS})
-message(STATUS "DAW_COMPILE_FLAGS = ${DAW_COMPILE_FLAGS}")
-message(STATUS "DAW_FLAG_LIST = ${DAW_FLAG_LIST}")
-
-   SET(listVarTmp "")
-   FOREACH(f ${DAW_COMPILE_FLAGS})
-      LIST(APPEND listVarTmp "-D${f}")
-   ENDFOREACH(f)
-   SET(DAW_COMPILE_FLAGS ${listVarTmp})
-message(STATUS "listVarTmp ${listVarTmp}")
-
-message(STATUS "DAW_FLAG_LIST = ${DAW_FLAG_LIST}")
-message(STATUS "DAW_COMPILE_FLAGS = ${DAW_COMPILE_FLAGS}")
-
-add_definitions(${DAW_COMPILE_FLAGS})
-
 
 if (IS_MINGW_BUILD)
   find_program(CV2PDB cv2pdb)
 endif()
+
+#SET(APP_COMMON_FLAGS VST_FORCE_DEPRECATED=0 USE_GLAD_GL_HEADERS GLM_FORCE_CXX14=1 PA_ENABLE_DEBUG_OUTPUT=1)
+SET(APP_COMMON_FLAGS VST_FORCE_DEPRECATED=0 USE_GLAD_GL_HEADERS GLM_FORCE_CXX14=1)
+if (WIN32)
+  SET(APP_COMMON_FLAGS ${APP_COMMON_FLAGS} PA_USE_ASIO=1 PA_USE_DS=1 PA_USE_WASAPI=1 PA_USE_WDMKS=1)
+endif(WIN32)
+if (UNIX)
+  SET(APP_COMMON_FLAGS ${APP_COMMON_FLAGS} PA_USE_ALSA=1)
+	set(APP_COMMON_FLAGS ${APP_COMMON_FLAGS} __cdecl="")
+endif(UNIX)
+
+FOREACH(app_common_flag ${APP_COMMON_FLAGS})
+  message(STATUS "Define: ${app_common_flag}")
+  add_definitions(-D${app_common_flag})
+ENDFOREACH(app_common_flag)
+
