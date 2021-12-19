73aa1a0b493af966bd57aabd556ddec375230990
Michael Hept <michael.hept@gmail.com>
2021-12-19 20:16:25 +0100

    Assume TLS was meant to be initialized on all singleton getters

--- src/host/tls.cpp
+++ src/host/tls.cpp
@@ -7,6 +7,7 @@
 #include "host/projectcontroller.h"
 #include "audiocache.h"
 #include "gui/drawwaveform.h"
+#include "thread.h"
 
 namespace daw_tls {
 	static thread_local tlsinstance tls;
@@ -24,46 +25,55 @@ namespace daw_tls {
 }
 
 plugindatabase_t* plugindatabase_t::getInstance() {
+	dbgassert(daw_tls::tls.tlsInitialized);
 	dbgassert(daw_tls::tls.pluginDatabase);
 	return daw_tls::tls.pluginDatabase;
 }
 vsthost* vsthost::getInstance()
 {
+	dbgassert(daw_tls::tls.tlsInitialized);
 	dbgassert(daw_tls::tls.host);
 	return daw_tls::tls.host;
 }
 project_controller_t* project_controller_t::get()
 {
+	dbgassert(daw_tls::tls.tlsInitialized);
 	dbgassert(daw_tls::tls.project);
 	return daw_tls::tls.project;
 }
 waveformrender* waveformrender::getInstance()
 {
-//	dbgassert(daw_tls::tls.waveform);
+	dbgassert(daw_tls::tls.tlsInitialized);
+	dbgassert(daw_tls::tls.waveform);
 	return daw_tls::tls.waveform;
 }
 audiocache* audiocache::getInstance()
 {
+	dbgassert(daw_tls::tls.tlsInitialized);
 	dbgassert(daw_tls::tls.audioCache);
 	return daw_tls::tls.audioCache;
 }
 MainCtrl* MainCtrl::get() {
-//	dbgassert(daw_tls::tls.mainCtrl);
+	dbgassert(daw_tls::tls.tlsInitialized);
+	dbgassert(daw_tls::tls.mainCtrl);
 	return daw_tls::tls.mainCtrl;
 }
 DawInstance* DawInstance::get() {
-//	dbgassert(daw_tls::tls.mainCtrl);
+	dbgassert(daw_tls::tls.tlsInitialized);
+	dbgassert(daw_tls::tls.mainCtrl);
 	if (daw_tls::tls.mainCtrl)
 		return &daw_tls::tls.mainCtrl->daw;
 	return nullptr;
 }
 audiohost* audiohost::getInstance()
 {
+	dbgassert(daw_tls::tls.tlsInitialized);
 	dbgassert(daw_tls::tls.audioHost);
 	return daw_tls::tls.audioHost;
 }
 midihost* midihost::getInstance()
 {
+	dbgassert(daw_tls::tls.tlsInitialized);
 	dbgassert(daw_tls::tls.midiHost);
 	return daw_tls::tls.midiHost;
 }
