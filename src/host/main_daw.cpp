2042fde99d1a8cee901887c5ff869288d6f0e023
Michael Hept <michael.hept@gmail.com>
2021-12-24 06:41:04 +0100

    Avoid use of strdup and cleanup main_daw.cpp

--- src/host/main_daw.cpp
+++ src/host/main_daw.cpp
@@ -1,5 +1,5 @@
-#include "str_util.h"
 #include "mainctrl.h"
+#include "str_util.h"
 #include <memory>
 #include <vector>
 
@@ -8,33 +8,36 @@ int startApplication(int argc, char* argv[]);
 std::shared_ptr<MainCtrl> mainctrl;
 std::shared_ptr<DawInstance> dawInstance;
 std::shared_ptr<AppCtrl> makeApp() {
-	if (!mainctrl) {
-		dawInstance = std::make_shared<DawInstance>();
-		DawInstance& dawRef = *dawInstance.get();
-		mainctrl = std::make_shared<MainCtrl>(dawRef);
-		std::vector<std::shared_ptr<AppCtrl>> companions;
-		dawRef.setMainControl(mainctrl.get());
-	}
-	return mainctrl;
+    if (!mainctrl) {
+        dawInstance = std::make_shared<DawInstance>();
+        DawInstance& dawRef = *dawInstance;
+        mainctrl = std::make_shared<MainCtrl>(dawRef);
+        std::vector<std::shared_ptr<AppCtrl>> companions;
+        dawRef.setMainControl(mainctrl.get());
+    }
+    return mainctrl;
 }
-
 
 void deleteApp() {
-	mainctrl = nullptr;
+    mainctrl = nullptr;
 }
+
+inline char* StringAsMStr(String& str) {
+    return str.empty() ? nullptr : &*str.begin();
+}
+
 int main(int argc, char* argv[]) {
-	std::vector<char*> vecArgs;
-	for (int i = 0; i < argc; i++) {
-		if (argv[i]) {
-			vecArgs.push_back(argv[i]);
-		}
-	}
-	vecArgs.push_back(strdup("-log"));
-	vecArgs.push_back(strdup("daw.log"));
-	int retVal = startApplication(static_cast<int>(vecArgs.size()), vecArgs.data());
-	// we manually end lifetime here before the at-exit destructors for static objects runs
-	// this is because the destructors have assertions that might throw/abort/print stacktraces that might not work as intended if triggered in at-exit destruction phase
-	mainctrl.reset();
-	dawInstance.reset();
-	return retVal;
+    std::vector<char*> vecArgs(&argv[0], &argv[argc]);
+    String strExtraArgs[] = {"-log", "daw.log"};
+    vecArgs.push_back(StringAsMStr(strExtraArgs[0]));
+    vecArgs.push_back(StringAsMStr(strExtraArgs[1]));
+    int retVal = startApplication(static_cast<int>(vecArgs.size()), vecArgs.data());
+    /**
+     * manually end lifetime here before the at-exit destructors for static objects runs.
+     * the destructors have assertions that might abort/print stacktraces that
+     * might not work as intended if triggered after main function returns
+     */
+    mainctrl.reset();
+    dawInstance.reset();
+    return retVal;
 }
