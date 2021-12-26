35389e02185a646448b983220d276e109275a1fe
Michael Hept <michael.hept@gmail.com>
2021-12-26 11:22:43 +0100

    Cleanup and format tracktimeline

--- src/gui/tracktimeline.h
+++ src/gui/tracktimeline.h
@@ -5,38 +5,37 @@
 #include "guicontainer.h"
 
 class guitrack_timeline : public guictr_base, grid_changed_cb {
-	scaled_grid& grid;
+    scaled_grid& grid;
+
 public:
-	ivec2 startDrag{ 0, 0 };
-	int dragDirection = -1;
-	double dragPosObjSpace = 0;
-	guitrack_timeline(scaled_grid& _grid)
-		: guictr_base(),
-		grid(_grid)
-	{
-		setCanMouseHit(true);
-		grid.addCallback(this);
-		padding = 0;
-	}
+    ivec2 startDrag{ 0, 0 };
+    int dragDirection      = -1;
+    double dragPosObjSpace = 0;
+    explicit guitrack_timeline(scaled_grid& _grid)
+        : guictr_base(),
+          grid(_grid) {
+        setCanMouseHit(true);
+        grid.addCallback(this);
+        padding = 0;
+    }
 
-	void handleDraggedBegin(MouseEvent& evt);
-	void handleDraggedMove(MouseEvent& evt);
-	void handleDraggedRelease(MouseEvent& evt);
-	void adjustZoom(float mousePosXScreenSpaceLocal, float disty);
-	void adjustOffset(float gridOffset);
-	void render(NVGcontext* vg);
-	void layout() {
-		for (guibase* gui : guis) {
-			gui->layout();
-		}
-	}
-	void gridChanged(scaled_grid& _grid) override {
-
-	}
-	virtual guibase* getFocusedControl() {
-		return this->parent;
-	}
-	virtual guibase* getFocusedContainer() {
-		return this->parent;
-	}
+    void handleDraggedBegin(MouseEvent& evt) override;
+    void handleDraggedMove(MouseEvent& evt) override;
+    void handleDraggedRelease(MouseEvent& evt) override;
+    void adjustZoom(float mousePosXScreenSpaceLocal, float disty);
+    void adjustOffset(float gridOffset);
+    void render(NVGcontext* vg) override;
+    void layout() override {
+        for (guibase* gui : guis) {
+            gui->layout();
+        }
+    }
+    void gridChanged(scaled_grid& _grid) override {
+    }
+    guibase* getFocusedControl() override {
+        return this->parent;
+    }
+    guibase* getFocusedContainer() override {
+        return this->parent;
+    }
 };
