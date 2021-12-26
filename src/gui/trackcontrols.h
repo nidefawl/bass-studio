57c079322452a1193cd6ab4c400ae45dc8430f85
Michael Hept <michael.hept@gmail.com>
2021-12-26 09:56:17 +0100

    Cleanup and format trackcontrols

--- src/gui/trackcontrols.h
+++ src/gui/trackcontrols.h
@@ -9,52 +9,51 @@
 class gui_track_subtrack_mixer;
 class gui_trackcontrols_title;
 
-class gui_track_controls: public guictr_base {
+class gui_track_controls : public guictr_base {
 public:
-	track_t* const m_track;
-	track_gui_entry_t* const m_trackentry;
+    track_t* const m_track;
+    track_gui_entry_t* const m_trackentry;
+
 private:
-	gui_trackcontrols_title* title;
-	guictr_base* mixer;
-	guictr_base* io;
-	std::vector<gui_track_subtrack_mixer*> automationLaneControls;
-	int dragMode = -1;
-	const int resizeHitY = 8;
-	const int DRAG_RESIZE = 1;
+    gui_trackcontrols_title* title;
+    guictr_base* mixer;
+    guictr_base* io;
+    std::vector<gui_track_subtrack_mixer*> automationLaneControls;
+    int dragMode          = -1;
+    const int resizeHitY  = 8;
+    const int DRAG_RESIZE = 1;
+
 public:
-	gui_track_controls(track_gui_entry_t* _entry);
-	~gui_track_controls();
-	bool isStaticContainer() {
-		return false;
-	}
-	void addSubtrackMixer(track_gui_entry_t* entry, gui_track_subtrack* al);
-	void removeSubtrackMixer(gui_track_subtrack* al);
-	void removeAllAutomationLanes(automatable_t* at, int32_t paramIdx);
-	void removeAllAutomationLanes(automatable_t* at);
-	void removeAllSubtracks();
-	void render(NVGcontext* vg) override;
-	void renderGroupHandle(NVGcontext* vg);
-	void handleDraggedBegin(MouseEvent& evt) {
-		DawInstance::get()->setSelectedTrack(m_track);
-		if (isResize(evt.relMousepos+this->pos)) {
-			dragMode = DRAG_RESIZE;
-		}
-	}
+    explicit gui_track_controls(track_gui_entry_t* _entry);
+    ~gui_track_controls() override;
+    bool isStaticContainer() override {
+        return false;
+    }
+    void addSubtrackMixer(track_gui_entry_t* entry, gui_track_subtrack* al);
+    void removeSubtrackMixer(gui_track_subtrack* al);
+    void removeAllAutomationLanes(automatable_t* at, int32_t paramIdx);
+    void removeAllAutomationLanes(automatable_t* at);
+    void removeAllSubtracks();
+    void render(NVGcontext* vg) override;
+    void renderGroupHandle(NVGcontext* vg);
+    void handleDraggedBegin(MouseEvent& evt) override {
+        DawInstance::get()->setSelectedTrack(m_track);
+        if (isResize(evt.relMousepos + this->pos)) {
+            dragMode = DRAG_RESIZE;
+        }
+    }
+    void handleDraggedMove(MouseEvent& evt) override;
+    void handleDraggedRelease(MouseEvent& evt) override {
+        dragMode = -1;
+    }
+    void handleRightClick(MouseEvent& evt) override;
 
-	void handleDraggedMove(MouseEvent& evt);
+    bool isResize(ivec2 mpos) {
+        int32_t resizeTopOrBottom = m_track->type < TRACK_TYPE_MIDI ? top() : bottom();
+        return mpos.y >= resizeTopOrBottom - resizeHitY && mpos.y < resizeTopOrBottom + resizeHitY;
+    }
 
-	void handleDraggedRelease(MouseEvent& evt) {
-		dragMode = -1;
-	}
-	void handleRightClick(MouseEvent& evt);
-	bool isResize(ivec2 mpos) {
-		int32_t resizeTopOrBottom = m_track->type < TRACK_TYPE_MIDI ? top() : bottom();
-		return mpos.y >= resizeTopOrBottom - resizeHitY
-				&& mpos.y < resizeTopOrBottom + resizeHitY;
-	}
-
-	bool mouseHitTest(ivec2 mpos, MouseHitEvt& evt) override;
-	void layout() override;
-	guibase* getTitle();
+    bool mouseHitTest(ivec2 mpos, MouseHitEvt& evt) override;
+    void layout() override;
+    guibase* getTitle();
 };
-
