1b533800cbf31b46bca14118d6f25517ab19039b
Michael Hept <michael.hept@gmail.com>
2022-02-20 11:34:41 +0100

    Update test_notes

--- test/test_notes.cpp
+++ test/test_notes.cpp
@@ -6,167 +6,7 @@
 #include "common/test_common.h"
 
 namespace {
-
-    void testDuplicates() {
-        TEST_BEGIN("testDuplicates");
-        test_rng rng;
-        clip_t clip;
-        clip_notes_t& notes  = clip.notes;
-        uint32_t nNotes      = 20;
-        uint32_t nDuplicates = 3;
-
-        for (uint32_t i = 0; i < nNotes; i++) {
-            note_t note;
-            note.time  = static_cast<tick_t>(2 + i * 4) * TICKS_16TH;
-            note.len   = 6 * TICKS_16TH;
-            note.pitch = 36 + (rng.randI() & 0x3F);
-            for (uint32_t j = 0; j < nDuplicates; j++) {
-                notes.add(note);
-            }
-        }
-        TEST_ASSERT_THROW(notes.m_list.size() == nNotes * nDuplicates);
-        notes.removeDuplicates();
-        TEST_ASSERT_THROW(notes.m_list.size() == nNotes * 1);
-        notes.updateBounds();
-        note_t noteStart;
-        note_t noteEnd;
-        noteStart.time  = 0;
-        noteStart.len   = TICKS_16TH;
-        noteStart.pitch = 0;
-        noteEnd.time    = TICKS_16TH * 1023;
-        noteEnd.len     = TICKS_16TH * 1024;
-        noteEnd.pitch   = 150;
-        notes.addSingle(noteStart);
-        notes.addSingle(noteEnd);
-        TEST_ASSERT_THROW(clip.notes.minNote.time == 0);
-        TEST_ASSERT_THROW(clip.notes.minNote.len == TICKS_16TH);
-        TEST_ASSERT_THROW(clip.notes.minNote.pitch == 0);
-        TEST_ASSERT_THROW(clip.notes.maxNote.time == TICKS_16TH * 1023);
-        TEST_ASSERT_THROW(clip.notes.maxNote.len == TICKS_16TH * 1024);
-        TEST_ASSERT_THROW(clip.notes.maxNote.pitch == 150);
-        notes.addSingle(noteStart);
-        notes.addSingle(noteStart);
-        notes.addSingle(noteEnd);
-        notes.addSingle(noteEnd);
-        notes.removeDuplicates();
-        TEST_ASSERT_THROW(clip.notes.minNote.time == 0);
-        TEST_ASSERT_THROW(clip.notes.minNote.len == TICKS_16TH);
-        TEST_ASSERT_THROW(clip.notes.minNote.pitch == 0);
-        TEST_ASSERT_THROW(clip.notes.maxNote.time == TICKS_16TH * 1023);
-        TEST_ASSERT_THROW(clip.notes.maxNote.len == TICKS_16TH * 1024);
-        TEST_ASSERT_THROW(clip.notes.maxNote.pitch == 150);
-        notes.removeSingle(noteStart);
-        TEST_ASSERT_THROW(clip.notes.minNote.time > 0);
-        notes.removeSingle(noteEnd);
-        TEST_ASSERT_THROW(clip.notes.maxNote.time < TICKS_16TH * 1023);
-        TEST_END();
-    }
-    template<typename T>
-    void testNoteProperties(T& notes) {
-
-        for (note_t& note : notes) {
-            TEST_ASSERT_THROW(note.time >= 0);
-            TEST_ASSERT_THROW(note.len >= 0);
-            TEST_ASSERT_THROW(note.pitch >= 0);
-            TEST_ASSERT_THROW(note.velocity >= 0);
-        }
-    }
-    void testTrackDataMidi() {
-        TEST_BEGIN("testNoteView");
-        trackdata_midi_t midi;
-        clip_t* clip        = new clip_t;
-        tick_t lastnoteTime = 0;
-        for (int i = 0; i < 256; i++) {
-            note_t note;
-            note.pitch    = 32 + (i % 10) * 3;
-            note.velocity = 64 + (-1 + (i & 1) * 2) * (i % 10) * 3;
-            note.time     = i * TICKS_BAR;
-            note.len      = i * 13 * 24;
-            lastnoteTime  = note.time;
-            clip->notes.add(note);
-        }
-        clip->notes.updateBounds();
-        midi.addClip(clip);
-        TEST_ASSERT_THROW(clip->notes.lastNote.time == lastnoteTime);
-        for (int iLoopEnabled = 0; iLoopEnabled < 2; iLoopEnabled++) {
-            for (int i = 0; i < 14; i++) {
-                clip->len         = clip->notes.lastNote.time;
-                clip->loopLen     = clip->len;
-                clip->loopEnabled = iLoopEnabled != 0;
-                clip->setDirty();
-                if (i > 0) {
-                    clip->loopLen = (i) *TICKS_BAR;
-                }
-
-                midi.sortClips();
-                std::vector<note_t> notes;
-                int step = 1;
-                for (int j = 0; j < TICKS_BAR * 1024; j += step) {
-                    clip->len     = j;
-                    clip->loopLen = clip->len;
-                    clip->setDirty();
-                    midi.sortClips();
-                    notes.clear();
-                    midi.getNotesInRange(math::max(0, j - TICKS_BAR * 64), j * 2, -1, -1, notes);
-                    testNoteProperties(notes);
-                    clip->setDirty();
-                    step += step / 60 + 1;
-                    clip->loopEnabled = true;
-                    tick_t rangeBegin;
-                    tick_t rangeEnd;
-                    rangeBegin = clip->notes.firstNote.start();
-                    rangeEnd   = clip->notes.lastNote.end();
-                    notes.clear();
-                    midi.getNotesInRange(rangeBegin, rangeEnd, -1, -1, notes);
-                    testNoteProperties(notes);
-                    for (note_t& note : notes) {
-                        tick_t noteStart = note.start();
-                        tick_t noteEnd   = note.end();
-                        TEST_ASSERT_THROW(noteEnd >= rangeBegin);
-                        TEST_ASSERT_THROW(noteStart < rangeEnd);
-                    }
-                    rangeBegin = clip->notes.firstNote.start() + 1;
-                    rangeEnd   = clip->notes.lastNote.end() - 1;
-                    notes.clear();
-                    midi.getNotesInRange(rangeBegin, rangeEnd, -1, -1, notes);
-                    testNoteProperties(notes);
-                    for (note_t& note : notes) {
-                        tick_t noteStart = note.start();
-                        tick_t noteEnd   = note.end();
-                        TEST_ASSERT_THROW(noteEnd >= rangeBegin);
-                        TEST_ASSERT_THROW(noteStart < rangeEnd);
-                    }
-                    rangeBegin = clip->notes.firstNote.start();
-                    rangeEnd   = clip->notes.lastNote.end();
-                    notes.clear();
-                    midi.getNotesInRange(rangeBegin, rangeEnd, rangeBegin + 32, rangeEnd - 32, notes);
-                    testNoteProperties(notes);
-                    for (note_t& note : notes) {
-                        tick_t noteStart = note.start();
-                        tick_t noteEnd   = note.end();
-                        TEST_ASSERT_THROW(noteEnd >= rangeBegin + 32);
-                        TEST_ASSERT_THROW(noteStart < rangeEnd - 32);
-                    }
-                    clip_notes_t& clipnotes = clip->getNoteViewRender();
-                    testNoteProperties(clipnotes.m_list);
-                    for (note_t& note : clipnotes.m_list) {
-                        tick_t noteStart = note.start();
-                        tick_t noteEnd   = note.end();
-                        TEST_ASSERT_THROW(noteStart >= clip->start());
-                        TEST_ASSERT_THROW(noteEnd <= clip->end());
-                    }
-                }
-            }
-        }
-        midi.removeClip(clip);
-        delete clip;
-        clip              = new clip_t;
-        clip->time        = 11354255;
-        clip->len         = 12145;
-        clip->offsetStart = 11272335;
-        clip->loopLen     = 11272192;
-        clip->loopEnabled = false;
-
+    tick_t populateWithManyNotes(clip_t* clip) {
         note_t n;
         n = note_t{ 52, 37, 11232734, 6152, 1 };
         clip->notes.add(n);
@@ -618,9 +458,198 @@ namespace {
         clip->notes.add(n);
         n = note_t{ 120, 37, 11322212, 6152, 1 };
         clip->notes.add(n);
+        return n.time;
+    }
+
+    void testDuplicates() {
+        TEST_BEGIN("testDuplicates");
+        test_rng rng;
+        clip_t clip;
+        clip_notes_t& notes  = clip.notes;
+        uint32_t nNotes      = 20;
+        uint32_t nDuplicates = 3;
+
+        for (uint32_t i = 0; i < nNotes; i++) {
+            note_t note;
+            note.time  = static_cast<tick_t>(2 + i * 4) * TICKS_16TH;
+            note.len   = 6 * TICKS_16TH;
+            note.pitch = 36 + (rng.randI() & 0x3F);
+            for (uint32_t j = 0; j < nDuplicates; j++) {
+                notes.add(note);
+            }
+        }
+        TEST_ASSERT_THROW(notes.m_list.size() == nNotes * nDuplicates);
+        notes.removeDuplicates();
+        TEST_ASSERT_THROW(notes.m_list.size() == nNotes * 1);
+        notes.updateBounds();
+        note_t noteStart;
+        note_t noteEnd;
+        noteStart.time  = 0;
+        noteStart.len   = TICKS_16TH;
+        noteStart.pitch = 0;
+        noteEnd.time    = TICKS_16TH * 1023;
+        noteEnd.len     = TICKS_16TH * 1024;
+        noteEnd.pitch   = 150;
+        notes.addSingle(noteStart);
+        notes.addSingle(noteEnd);
+        TEST_ASSERT_THROW(clip.notes.minNote.time == 0);
+        TEST_ASSERT_THROW(clip.notes.minNote.len == TICKS_16TH);
+        TEST_ASSERT_THROW(clip.notes.minNote.pitch == 0);
+        TEST_ASSERT_THROW(clip.notes.maxNote.time == TICKS_16TH * 1023);
+        TEST_ASSERT_THROW(clip.notes.maxNote.len == TICKS_16TH * 1024);
+        TEST_ASSERT_THROW(clip.notes.maxNote.pitch == 150);
+        notes.addSingle(noteStart);
+        notes.addSingle(noteStart);
+        notes.addSingle(noteEnd);
+        notes.addSingle(noteEnd);
+        notes.removeDuplicates();
+        TEST_ASSERT_THROW(clip.notes.minNote.time == 0);
+        TEST_ASSERT_THROW(clip.notes.minNote.len == TICKS_16TH);
+        TEST_ASSERT_THROW(clip.notes.minNote.pitch == 0);
+        TEST_ASSERT_THROW(clip.notes.maxNote.time == TICKS_16TH * 1023);
+        TEST_ASSERT_THROW(clip.notes.maxNote.len == TICKS_16TH * 1024);
+        TEST_ASSERT_THROW(clip.notes.maxNote.pitch == 150);
+        notes.removeSingle(noteStart);
+        TEST_ASSERT_THROW(clip.notes.minNote.time > 0);
+        notes.removeSingle(noteEnd);
+        TEST_ASSERT_THROW(clip.notes.maxNote.time < TICKS_16TH * 1023);
+        TEST_END();
+    }
+    template<typename T>
+    void testNoteProperties(T& notes) {
+
+        for (note_t& note : notes) {
+            TEST_ASSERT_THROW(note.time >= 0);
+            TEST_ASSERT_THROW(note.len >= 0);
+            TEST_ASSERT_THROW(note.pitch >= 0);
+            TEST_ASSERT_THROW(note.velocity >= 0);
+        }
+    }
+    void testTrackDataMidi() {
+        TEST_BEGIN("testNoteView");
+        // Test trackdata_midi_t destruction after copy
+        {
+            trackdata_midi_t midiCopyOuter;
+            {
+                trackdata_midi_t midiDataInner;
+                midiDataInner.addClip(new clip_t);
+                midiCopyOuter = midiDataInner;
+                midiDataInner.deleteClips(nullptr);
+            }
+            midiCopyOuter.deleteClips(nullptr);
+        }
+        clip_t* clip        = new clip_t;
+        tick_t lastnoteTime = 0;
+        for (int i = 0; i < 256; i++) {
+            note_t note;
+            note.pitch    = 32 + (i % 10) * 3;
+            note.velocity = 64 + (-1 + (i & 1) * 2) * (i % 10) * 3;
+            note.time     = i * TICKS_BAR;
+            note.len      = i * 13 * 24;
+            lastnoteTime  = note.time;
+            clip->notes.add(note);
+        }
+        clip->notes.updateBounds();
+        
+        // Test clip_t bounds correctness
+        TEST_ASSERT_THROW(clip->notes.lastNote.time == lastnoteTime);
+
+        trackdata_midi_t midi;
+        midi.addClip(clip);
+
+
+        // Test trackdata_midi_t equality after copy
+
+        trackdata_midi_t midiCopy;
+        midiCopy = midi;
+        trackdata_midi_t midiCopy2 = midiCopy;
+        TEST_ASSERT_EQUAL(midiCopy2.start(), midi.start());
+        TEST_ASSERT_EQUAL(midiCopy2.end(), midi.end());
+        midiCopy.deleteClips(nullptr);
+        midiCopy2.deleteClips(nullptr);
+        for (int iLoopEnabled = 0; iLoopEnabled < 2; iLoopEnabled++) {
+            for (int i = 0; i < 14; i++) {
+                clip->len         = clip->notes.lastNote.time;
+                clip->loopLen     = clip->len;
+                clip->loopEnabled = iLoopEnabled != 0;
+                clip->setDirty();
+                if (i > 0) {
+                    clip->loopLen = (i) *TICKS_BAR;
+                }
+
+                midi.sortClips();
+                std::vector<note_t> notes;
+                int step = 1;
+                for (int j = 0; j < TICKS_BAR * 1024; j += step) {
+                    clip->len     = j;
+                    clip->loopLen = clip->len;
+                    clip->setDirty();
+                    midi.sortClips();
+                    notes.clear();
+                    midi.getNotesInRange(math::max(0, j - TICKS_BAR * 64), j * 2, -1, -1, notes);
+                    testNoteProperties(notes);
+                    clip->setDirty();
+                    step += step / 60 + 1;
+                    clip->loopEnabled = true;
+                    tick_t rangeBegin;
+                    tick_t rangeEnd;
+                    rangeBegin = clip->notes.firstNote.start();
+                    rangeEnd   = clip->notes.lastNote.end();
+                    notes.clear();
+                    midi.getNotesInRange(rangeBegin, rangeEnd, -1, -1, notes);
+                    testNoteProperties(notes);
+                    for (note_t& note : notes) {
+                        tick_t noteStart = note.start();
+                        tick_t noteEnd   = note.end();
+                        TEST_ASSERT_THROW(noteEnd >= rangeBegin);
+                        TEST_ASSERT_THROW(noteStart < rangeEnd);
+                    }
+                    rangeBegin = clip->notes.firstNote.start() + 1;
+                    rangeEnd   = clip->notes.lastNote.end() - 1;
+                    notes.clear();
+                    midi.getNotesInRange(rangeBegin, rangeEnd, -1, -1, notes);
+                    testNoteProperties(notes);
+                    for (note_t& note : notes) {
+                        tick_t noteStart = note.start();
+                        tick_t noteEnd   = note.end();
+                        TEST_ASSERT_THROW(noteEnd >= rangeBegin);
+                        TEST_ASSERT_THROW(noteStart < rangeEnd);
+                    }
+                    rangeBegin = clip->notes.firstNote.start();
+                    rangeEnd   = clip->notes.lastNote.end();
+                    notes.clear();
+                    midi.getNotesInRange(rangeBegin, rangeEnd, rangeBegin + 32, rangeEnd - 32, notes);
+                    testNoteProperties(notes);
+                    for (note_t& note : notes) {
+                        tick_t noteStart = note.start();
+                        tick_t noteEnd   = note.end();
+                        TEST_ASSERT_THROW(noteEnd >= rangeBegin + 32);
+                        TEST_ASSERT_THROW(noteStart < rangeEnd - 32);
+                    }
+                    clip_notes_t& clipnotes = clip->getNoteViewRender();
+                    testNoteProperties(clipnotes.m_list);
+                    for (note_t& note : clipnotes.m_list) {
+                        tick_t noteStart = note.start();
+                        tick_t noteEnd   = note.end();
+                        TEST_ASSERT_THROW(noteStart >= clip->start());
+                        TEST_ASSERT_THROW(noteEnd <= clip->end());
+                    }
+                }
+            }
+        }
+        midi.removeClip(clip);
+        delete clip;
+        clip              = new clip_t;
+        clip->time        = 11354255;
+        clip->len         = 12145;
+        clip->offsetStart = 11272335;
+        clip->loopLen     = 11272192;
+        clip->loopEnabled = false;
+
+        lastnoteTime = populateWithManyNotes(clip);
+
         clip->setDirty();
         clip->notes.updateBounds();
-        lastnoteTime = n.time;
         TEST_ASSERT_THROW(clip->notes.lastNote.time == lastnoteTime);
 
         midi.addClip(clip);
