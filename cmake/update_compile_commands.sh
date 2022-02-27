a564306ce4368efa0e3eb816e44471db00fa4c6b
Michael Hept <michael.hept@gmail.com>
2022-02-27 09:16:24 +0100

    Update compile commands script

--- cmake/update_compile_commands.sh
+++ cmake/update_compile_commands.sh
@@ -2,6 +2,16 @@
 BASEDIR=$(dirname "$BASH_SOURCE")
 TMP_BUILD=$(realpath "$BASEDIR/../.cache/cmake_commands")
 PROJECT_DIR=$(realpath "$BASEDIR/..")
+builtin type -P "ninja" &> /dev/null
+[[ $? -ne 0 ]] && echo "ninja not found" && exit 1
+builtin type -P "cmake" &> /dev/null
+[[ $? -ne 0 ]] && echo "cmake not found" && exit 1
+builtin type -P "clang" &> /dev/null
+[[ $? -ne 0 ]] && echo "clang not found" && exit 1
+builtin type -P "clang++" &> /dev/null
+[[ $? -ne 0 ]] && echo "clang++ not found" && exit 1
+CC=clang
+CXX=clang++
 cmake --log-level=WARNING -GNinja -S"${PROJECT_DIR}" -B"${TMP_BUILD}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_DISABLE_PRECOMPILE_HEADERS=ON -DCMAKE_UNITY_BUILD=OFF $*
 if [ ! $? -eq 0 ]; then
     echo "CMake failed"
