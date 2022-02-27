a6b2776c470429ae543f2cafe1420b969ddb8ce5
nidefawl <michael.hept@gmail.com>
2022-02-27 16:10:36 +0100

    Remove file from repo

--- local-update-cc.sh
+++
@@ -1,2 +0,0 @@
-PATH=/c/dev/WIN32PATH:/c/dev/bin/llvm-mingw-20220209-ucrt-x86_64/bin:/c/dev/bin/cmake-3.22.1/bin:$PATH
-./cmake/update_compile_commands.sh -DPROJECT_DEPS_INSTALL_PATH:PATH=C:/dev/daw-deps-install/llvmmingw14 -DPROJECT_DEPS_PATH:PATH=D:/dev/daw-deps
