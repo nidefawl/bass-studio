3f6e7f391c342bae050ce869fe6e41d584046e38
Michael Hept <michael.hept@gmail.com>
2022-02-28 21:18:42 +0100

    Update compile commands script

--- cmake/update_compile_commands.sh
+++ cmake/update_compile_commands.sh
@@ -12,11 +12,11 @@ builtin type -P "ninja" &> /dev/null
 [[ $? -ne 0 ]] && echo "ninja not found" && exit 1
 builtin type -P "cmake" &> /dev/null
 [[ $? -ne 0 ]] && echo "cmake not found" && exit 1
-builtin type -P "clang" &> /dev/null
-[[ $? -ne 0 ]] && echo "clang not found" && exit 1
-builtin type -P "clang++" &> /dev/null
-[[ $? -ne 0 ]] && echo "clang++ not found" && exit 1
-CC=clang CXX=clang++ cmake --log-level=WARNING -GNinja -S"${PROJECT_DIR}" -B"${TMP_BUILD}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_DISABLE_PRECOMPILE_HEADERS=ON -DCMAKE_UNITY_BUILD=OFF -DPROJECT_WORKING_DIR:PATH=../../run $*
+builtin type -P "$CC" &> /dev/null
+[[ $? -ne 0 ]] && echo "CC not found" && exit 1
+builtin type -P "$CXX" &> /dev/null
+[[ $? -ne 0 ]] && echo "CXX not found" && exit 1
+cmake --log-level=WARNING -GNinja -S"${PROJECT_DIR}" -B"${TMP_BUILD}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_DISABLE_PRECOMPILE_HEADERS=ON -DCMAKE_UNITY_BUILD=OFF -DPROJECT_WORKING_DIR:PATH=../../run $*
 if [ ! $? -eq 0 ]; then
     echo "CMake failed"
     exit $exit_status
