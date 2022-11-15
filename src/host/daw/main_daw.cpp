426961508ca4576e9f7c3ca6beabac8d1afa38b0
Michael Hept <637382+nidefawl@users.noreply.github.com>
2022-11-15 12:19:48 +0100

    Update logfile name

--- src/host/daw/main_daw.cpp
+++ src/host/daw/main_daw.cpp
@@ -36,7 +36,7 @@ public:
 int main(int argc, char* argv[]) {
     DawAppInstService instService;
     std::vector<String> vecArgs(&argv[0], &argv[argc]);
-    vecArgs.insert(vecArgs.end(), {"--logfile", "daw.log", "--log", "TRACE"});
+    vecArgs.insert(vecArgs.end(), {"--logfile", String(BuildInfo::PRODUCT_NAME_LOWER) + ".log"});
     int retVal = startApplication(vecArgs, instService);
     return retVal;
 }
