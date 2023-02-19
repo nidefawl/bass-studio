672144b294219e84a9683c15b0a74014cd419996
nidefawl <637382+nidefawl@users.noreply.github.com>
2023-02-19 22:36:57 +0100

    Fix stereowidth getModuleType

--- src/plugins/stereowidth/stereowidth-plugin.h
+++ src/plugins/stereowidth/stereowidth-plugin.h
@@ -15,7 +15,7 @@ public:
     const float MTR_CEIL      = 24.0f;
     explicit module_stereowidth(int32_t _projectGlobalId, IHostCallback* _hostCallback);
 
-    int getModuleType() override { return PLUGIN_TYPE_GAIN; };
+    int getModuleType() override { return PLUGIN_TYPE_STEREO_WIDTH; };
     void process(const DAW::Host::Host* const host, AudioBlock* in, AudioBlock* out, double tick, double samplePos, int32_t numSamples, playback_state state) override;
     param_converted_t convertParamValueDisplay(int32_t idx, const param_unit_t& displayValue) override;
     param_unit_t convertParamValueToDisplay(int32_t idx, float value) override;
