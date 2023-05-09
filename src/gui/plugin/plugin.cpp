1d5800e60847b7a52c78bc6334515640d01f9fc4
nidefawl <637382+nidefawl@users.noreply.github.com>
2023-05-09 07:02:33 +0200

    Fix for enable button color

--- src/gui/plugin/plugin.cpp
+++ src/gui/plugin/plugin.cpp
@@ -176,8 +176,12 @@ guiplugin::guiplugin(effectbase* _effect)
     buttonBypass.setLabel("Bypass");
     buttonBypass.colorActive = GuiColor::COL_BTN_BG_BYPASS_ACTIVE;
     buttonBypass.icon        = ICON_BYPASS;
-    buttonBypass.fnGetState  = [_effect]() {
-        return _effect->getParam(PARAM_ENABLE)->getValueAutomated() > 0;
+    buttonBypass.fnGetState  = [this, _effect]() {
+        auto flags = buttonBypass.getStateFlags();
+        if (flags & FLG_IS_AUTOMATED && !(flags & FLG_IS_AUTOMATION_INACTIVE)) {
+            return _effect->getParam(PARAM_ENABLE)->getValueAutomated() > 0;
+        }
+        return _effect->getParam(PARAM_ENABLE)->getValue() > 0;
     };
     //buttonBypass.setTint(0x80c040);
     buttonDelete.setLabel("Remove");
