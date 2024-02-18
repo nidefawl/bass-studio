7ff57a73654266b6115e54a47b32ce6bd16893da
nidefawl <637382+nidefawl@users.noreply.github.com>
2024-02-18 03:40:23 +0100

    Implement DeleteDirectory on linux

--- src/platform/linux/fileio.cpp
+++ src/platform/linux/fileio.cpp
@@ -243,6 +243,13 @@ IOFile* IOFile::openFile(const String& filename, OpenFileMode mode) {
     return new IOFile(impl);
 }
 
+int IsDirectory(const char *path) {
+   struct stat statbuf{};
+   if (stat(path, &statbuf) != 0)
+       return 0;
+   return S_ISDIR(statbuf.st_mode);
+}
+
 bool CreateDirectoryIfNotExists(const String& DirPath) {
     String partPath = "";
     do {
@@ -261,6 +268,77 @@ bool CreateDirectoryIfNotExists(const String& DirPath) {
         }
     } while (partPath.size() < DirPath.size());
     return true;
+}
+
+bool DeleteDirectory(const String& DirPath, bool bRecursive) {
+    if (!assert_expr(!DirPath.empty())) {
+        return false;
+    }
+
+    const char* dir = DirPath.c_str();
+
+    int ret = 0;
+    FTS *ftsp = NULL;
+    FTSENT *curr;
+
+    // Cast needed (in C) because fts_open() takes a "char * const *", instead
+    // of a "const char * const *", which is only allowed in C++. fts_open()
+    // does not modify the argument.
+    char *files[] = { (char *) dir, NULL };
+
+    // FTS_NOCHDIR  - Avoid changing cwd, which could cause unexpected behavior
+    //                in multithreaded programs
+    // FTS_PHYSICAL - Don't follow symlinks. Prevents deletion of files outside
+    //                of the specified directory
+    // FTS_XDEV     - Don't cross filesystem boundaries
+    ftsp = fts_open(files, FTS_NOCHDIR | FTS_PHYSICAL | FTS_XDEV, NULL);
+    if (!ftsp) {
+        fprintf(stderr, "%s: fts_open failed: %s\n", dir, strerror(errno));
+        ret = -1;
+        goto finish;
+    }
+
+    while ((curr = fts_read(ftsp))) {
+        switch (curr->fts_info) {
+        case FTS_NS:
+        case FTS_DNR:
+        case FTS_ERR:
+            fprintf(stderr, "%s: fts_read error: %s\n",
+                    curr->fts_accpath, strerror(curr->fts_errno));
+            break;
+
+        case FTS_DC:
+        case FTS_DOT:
+        case FTS_NSOK:
+            // Not reached unless FTS_LOGICAL, FTS_SEEDOT, or FTS_NOSTAT were
+            // passed to fts_open()
+            break;
+
+        case FTS_D:
+            // Do nothing. Need depth-first search, so directories are deleted
+            // in FTS_DP
+            break;
+
+        case FTS_DP:
+        case FTS_F:
+        case FTS_SL:
+        case FTS_SLNONE:
+        case FTS_DEFAULT:
+            if (remove(curr->fts_accpath) < 0) {
+                fprintf(stderr, "%s: Failed to remove: %s\n",
+                        curr->fts_path, strerror(curr->fts_errno));
+                ret = -1;
+            }
+            break;
+        }
+    }
+
+finish:
+    if (ftsp) {
+        fts_close(ftsp);
+    }
+
+    return ret == 0;
 }
 
 #endif
