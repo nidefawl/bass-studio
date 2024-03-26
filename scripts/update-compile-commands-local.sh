f42e09110b5ee73a2228ffc58990cd10d2b4d173
nidefawl <637382+nidefawl@users.noreply.github.com>
2024-03-26 23:32:56 +0100

    Update clangd helper script

--- scripts/update-compile-commands-local.sh
+++ scripts/update-compile-commands-local.sh
@@ -6,5 +6,6 @@ if [ -n "$ZSH_VERSION" ]; then
 else
   SCRIPT_PATH=${BASH_SOURCE[0]}
 fi
-BASEDIR=$(dirname "$SCRIPT_PATH")
-bash $BASEDIR/update-compile-commands.sh -DPROJECT_DEPS_INSTALL_PATH=~/dev/build-deps/install
+BASEDIR=$(dirname "$BASH_SOURCE")
+$BASEDIR/update-compile-commands.sh -DPROJECT_DEPS_INSTALL_PATH:PATH=C:/dev/daw-deps-install/llvm-mingw-20240221-ucrt-x86_64 -DPROJECT_DEPS_PATH:PATH=C:/dev/daw-deps
+ 
