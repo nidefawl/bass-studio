a9a8e1e4eee8e52bcf19d210a2e7a1b348362e13
nidefawl <637382+nidefawl@users.noreply.github.com>
2024-06-10 15:23:30 +0200

    mark GlfwContextSwitch constructor as explicit

--- src/gl/gl_context.hpp
+++ src/gl/gl_context.hpp
@@ -5,7 +5,7 @@ class GlfwContextSwitch {
     GLFWwindow* const window;
     GLFWwindow* const curContext;
 public:
-    GlfwContextSwitch(GLFWwindow* window) : window(window), curContext(glfwGetCurrentContext()) {
+    explicit GlfwContextSwitch(GLFWwindow* window) : window(window), curContext(glfwGetCurrentContext()) {
         if (curContext != window) {
             glfwMakeContextCurrent(window);
         }
