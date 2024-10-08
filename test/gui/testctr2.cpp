c841c9d6bc275e6e5a2c12a681e60b17af14a571
nidefawl <637382+nidefawl@users.noreply.github.com>
2024-10-08 20:29:24 +0200

    Add test code for font metrics

--- test/gui/testctr2.cpp
+++ test/gui/testctr2.cpp
@@ -1,41 +1,105 @@
 
 #include "gui/views/controls.h"
+#include "platform.h"
 #include "types.h"
 #include "str_util.h"
 #include "basectrl.h"
 #include "gui/container/container.h"
 #include "logging.h"
+#include <nanovg.h>
 
 namespace {
-class guictr_bla : public guictr_base {
+class guictr_test_inner : public guictr_base {
 public:
-    guictr_bla()
+    guictr_test_inner()
         : guictr_base()
     {
         setLayoutMode(autolayout_mode::LAYOUT_VERTICAL);
     }
-    ~guictr_bla() override {
+    ~guictr_test_inner() override {
         removeGuis();
+    }
+    void render(NVGcontext* vg) override {
+        if (!setScissorTransform(vg)) {
+            return;
+        }
+        nvgBeginPath(vg);
+        nvgRect(vg, 0, 0, size.x, size.y);
+        nvgFillColor(vg, nvgRGBA(0, 255, 0, 128));
+        nvgFill(vg);
+        // translate to center
+        nvgSave(vg);
+        nvgTranslate(vg, size.x * 0.5f, size.y * 0.5f);
+        auto fs = 64.0f;
+        auto fontScale = fs * theme->getFloat(GuiConstant::CONST_FONT_SCALE);
+        nvgFontSize(vg, fontScale);
+        UIFont::font_instance instance = theme->getFont(UIFont::FONT_TEXTFIELD);
+        UIFont::bindFont(vg, instance);
+        nvgTextAlign(vg, NVG_ALIGN_LEFT | NVG_ALIGN_MIDDLE);
+        auto aOrB = (getTimeMillis()/1000) % 3;
+        String tmpStr = "";
+        switch (aOrB) {
+            default:
+            case 0:
+                tmpStr = " ";
+                break;
+            case 1:
+                tmpStr = " a";
+                break;
+            case 2:
+                tmpStr = " a ";
+                break;
+        }
+
+        float textBounds[4]{ 0 };
+        float textWidth = nvgTextBounds(vg, 0, 0, tmpStr.c_str(), nullptr, textBounds);
+
+        // draw text bounds
+        nvgBeginPath(vg);
+        nvgRect(vg, textBounds[0], textBounds[1], textBounds[2] - textBounds[0], textBounds[3] - textBounds[1]);
+        nvgFillColor(vg, nvgRGBA(255, 0, 0, 128));
+        nvgFill(vg);
+        // draw text
+        nvgFillColor(vg, nvgRGBA(0, 0, 0, 255));
+        float xret = nvgText(vg, 0, 0, tmpStr.c_str(), nullptr);
+
+        // draw understrike using textWidth
+        nvgBeginPath(vg);
+        nvgMoveTo(vg, 0, 0);
+        nvgLineTo(vg, textWidth, 0);
+        nvgStrokeColor(vg, nvgRGBA(255, 255, 0, 255));
+        nvgStrokeWidth(vg, 1.0f);
+        nvgStroke(vg);
+
+        // second understrike using xret
+        nvgBeginPath(vg);
+        nvgMoveTo(vg, 0, 4);
+        nvgLineTo(vg, xret, 4);
+        nvgStrokeColor(vg, nvgRGBA(0, 255, 255, 255));
+        nvgStrokeWidth(vg, 1.0f);
+        nvgStroke(vg);
+
+        nvgRestore(vg);
     }
 };
 }
-class guictr_test2 : public guictr_base {
-    guictr_bla ctr;
+class guictr_test_outer : public guictr_base {
+    guictr_test_inner ctr;
 public:
-    guictr_test2();
-    ~guictr_test2() override;
+    guictr_test_outer();
+    ~guictr_test_outer() override;
     void layout() override;
 };
 
-guictr_test2::guictr_test2() : guictr_base() {
+guictr_test_outer::guictr_test_outer() : guictr_base() {
     setBackgroundRendered(true);
     add(&ctr);
 }
-guictr_test2::~guictr_test2() {
+guictr_test_outer::~guictr_test_outer() {
     removeGuis();
 }
 
-void guictr_test2::layout() {
+void guictr_test_outer::layout() {
     ivec2 cs          = getSizeContent();
     ctr.pos = {};
     ctr.size = cs;
@@ -45,5 +109,5 @@ void guictr_test2::layout() {
 }
 
 guictr_base* makeGuiTestCtr2() {
-    return new guictr_test2();
+    return new guictr_test_outer();
 }
