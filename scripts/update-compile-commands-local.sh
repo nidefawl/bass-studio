b86287de651c50f6cf496d605ad44fb3b7c0ccf1
nidefawl <637382+nidefawl@users.noreply.github.com>
2025-11-09 03:02:56 +0100

    Updaet compile-commands scripts

--- scripts/update-compile-commands-local.sh
+++ scripts/update-compile-commands-local.sh
@@ -6,6 +6,6 @@ if [ -n "$ZSH_VERSION" ]; then
 else
   SCRIPT_PATH=${BASH_SOURCE[0]}
 fi
-BASEDIR=$(dirname "$BASH_SOURCE")
-$BASEDIR/update-compile-commands.sh -DPROJECT_DEPS_INSTALL_PATH:PATH=C:/dev/daw-deps-install/llvm-mingw-20240221-ucrt-x86_64 -DPROJECT_DEPS_PATH:PATH=C:/dev/daw-deps
+BASEDIR=$(dirname "$SCRIPT_PATH")
+$BASEDIR/update-compile-commands.sh -DPROJECT_DEPS_INSTALL_PATH:PATH=/data/dev/build-deps/install -DPROJECT_DEPS_PATH:PATH=/data/dev/daw-deps
  
